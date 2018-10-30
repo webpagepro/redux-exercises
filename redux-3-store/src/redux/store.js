ee5a1cc0843ce3594f20f110cb7d75e303462793
John <jaboyak@gmail.com>
2018-10-29 19:31:25 -0700

    updated 3-store

--- redux-3-store/src/redux/store.js
+++ redux-3-store/src/redux/store.js
@@ -1,8 +1,1 @@
-import { createStore, combineReducers } from 'redux'
-import todos from './todos.reducer'
-
-let rootReducer = combineReducers({
-  todos: todos
-})
-
-export default createStore(rootReducer)
+// Create your store here!
